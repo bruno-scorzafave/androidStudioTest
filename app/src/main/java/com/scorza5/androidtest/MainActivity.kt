dfb6288c70e85e50b4a8527dc90bc2e7d0096993
Bruno Scorzafave <bruno.scorzafave@aluno.ufabc.edu.br>
2021-08-27 19:33:28 -0300

    Muita coisa mudou não sei quando foi o commit

--- app/src/main/java/com/scorza5/androidtest/MainActivity.kt
+++ app/src/main/java/com/scorza5/androidtest/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.scorza5.androidtest
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.loader.app.LoaderManager.LoaderCallbacks
+import androidx.loader.content.Loader
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderCallbacks<Cursor> {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //nada
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
---
+++ app/src/main/java/com/scorza5/androidtest/MainViewModel.kt
@@ -0,0 +1,25 @@
+package com.scorza5.androidtest
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    //pegar do banco no lambda?
+    var mContador = MutableLiveData<String>().apply { value = contador.toString() }
+    private var contador: Int = 0
+
+    private fun setmContador(){
+        mContador.value = contador.toString()
+    }
+
+    private fun validaContador(){
+        contador++
+        if (contador > 5){
+            contador = 0
+        }
+
+        setmContador()
+    }
+
+    fun Contador (){ validaContador() }
+}
